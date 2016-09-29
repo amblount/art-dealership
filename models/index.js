4db9eec54a0b0ab4298492dc1d8815ad4655b45c
Alivia Blount <aliviablount@gmail.com>
2016-09-29 16:50:18 -0700

    discard model keys attached to export that were unused

--- models/index.js
+++ models/index.js
@@ -2,9 +2,11 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/art_dealership");
 // open connection to db
 module.exports = {
-  Artist: require('./artist').Artist,
-  Artwork: require('/artwork').Artwork,
-  ArtistArtwork: require('./artist_artwork').ArtistArtwork
-}
+  Artist: require('./artist'),
+  Artwork: require('./artwork'),
+  ArtistArtwork: require('./artist_artwork')
+};
 // require all models
 // export all models
+console.log(Object.keys(require('./artist'))
+// console.log()
